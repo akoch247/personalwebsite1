18539af3d6e8b0a19fd882df8b9cb0e91e662938
Adriana Koch <akoch@oberlin.edu>
2025-08-31 16:48:01 -0400

    add more finishing touches to layout

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,15 +4,15 @@ import "./Home.css";
 
 export default function Home() {
     return (
-        <div className="d-flex flex-column flex-lg-row align-items-center justify-content-center">
+        <div className="d-flex flex-column flex-lg-row align-items-start justify-content-center">
             <motion.div 
                 initial={{ opacity: 0, x: -30 }} 
                 animate={{ opacity: 1, x: 0 }} 
                 transition={{ duration: 0.7 }}
                 className="text-center text-lg-start"
             >
-                <h1 className="fw-bold display-5">
-                    Hi, I'm <span className="text-primary">Adriana Koch</span><br />
+                <h1 className="display-5 mt-5">
+                    Hi, I'm Adriana Koch <br />
                     Full Stack Developer
                 </h1>
                 <p className="lead mt-3">
@@ -20,8 +20,8 @@ export default function Home() {
                     from backend to frontend.
                 </p>
                 <div className="mt-4">
-                    <a href="/Projects" className="btn btn-primary btn-lg me-3">View Projects</a>
-                    <a href="/Contact" className="btn btn-outline-light btn-lg">Contact Me</a>
+                    <a href="/Projects" className="btn btn-primary btn-lg me-2">View Projects</a>
+                    <a href="/Contact" className="btn btn-outline-light btn-lg contact-btn">Contact Me</a>
                 </div>
             </motion.div>
 
@@ -30,13 +30,13 @@ export default function Home() {
                 initial={{ opacity: 0, x: 30 }} 
                 animate={{ opacity: 1, x: 0 }} 
                 transition={{ duration: 0.7, delay: 0.2 }}
-                className="mt-5 mt-lg-0 ms-lg-4"
+                
             >
                 <img
                      src="/laptop-illustration1.png" 
                      alt="Laptop Illustration" 
-                     className="img-fluid computer-img"
-                     style={{ maxWidth: "700px" }}
+                     className="img-fluid computer-img mt-5"
+                     style={{ maxWidth: "450px" }}
                 />
             </motion.div>
         </div>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,32 +1,20 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
-import DarkVeil from "./DarkVeil";
 import "./Layout.css";
 import Particles from "./Particles";
 
 export default function Layout() {
     return (
-        <div style={{ position: "relative", minHeight: "100vh" }}>
-            <div style={{
-                position: "fixed", 
-                top: 0, 
-                left: 0, 
-                width: "100%", 
-                height: "100%", 
-                zIndex: -1
-            }}>
-                <DarkVeil />
-            </div>
-
+        <div className="layout-background">
             <div className="container-fluid d-flex justify-content-center align-items-center py-5">
-                <div className="card custom-card text-light shadow-lg rounded-4 px-5 pt-2 border-0 w-90">
+                <div className="card custom-card text-light shadow-lg rounded-4 px-5 pt-3 border-0 w-90">
                     <div style={{ width: '100%', height: '600px', position: 'absolute' }}>
                         <Particles
-                            particleColors={['#1D1773', '#1D1773']}
-                            particleCount={200}
+                            particleColors={['#1D1773', '#0d1117']}
+                            particleCount={400}
                             particleSpread={10}
-                            speed={0.05}
+                            speed={0.08}
                             particleBaseSize={100}
                             moveParticlesOnHover={false}
                             alphaParticles={false}
@@ -39,6 +27,6 @@ export default function Layout() {
                     </main>
                 </div>
             </div>
-        </div>
-    );
+        </div>     
+        );
 }
